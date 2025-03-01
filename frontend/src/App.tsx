6193f29f2dd6d784767782c172643d4f105ced59
rodzelte <zythonix.te@gmail.com>
2025-03-01 12:48:50 +0800

    tangina shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,9 @@
-import { useEffect, useState } from "react";
-import { getTasks } from "./services/api";
+import { Button } from "./components/ui/button";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    getTasks()
-      .then((data) => {
-        console.log("Tasks:", data);
-        setTasks(data.tasks || data.getTasks);
-      })
-      .catch((error) => console.error("Error fetching tasks:", error));
-  }, []);
-
   return (
     <div>
-      <div>
-        <h1>Tasks</h1>
-        {tasks.length > 0 ? (
-          <ul>
-            {tasks.map((task) => (
-              <li key={task.id}>
-                {task.title || task.name || JSON.stringify(task)}
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No tasks available</p>
-        )}
-      </div>
+      <Button variant={"destructive"}>Yawa Walay Hover</Button>
     </div>
   );
 }
