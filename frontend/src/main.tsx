2c46ad2992b1ed287b087cf6cb3b7bbc68a64991
rodzelte <zythonix.te@gmail.com>
2025-03-02 13:02:25 +0800

    lol

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/layout/Sidebar";
+// import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+// import { AppSidebar } from "@/components/layout/Sidebar";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
