14396c02f76bc1ccc14381376fb71fb13c7e355b
rodzelte <zythonix.te@gmail.com>
2025-03-04 17:08:03 +0800

    initial commit UI still not fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,31 +5,38 @@ import { Routes, Route } from "react-router";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
+import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
+import { AppSidebar } from "./components/app-sidebar";
 
 export default function App() {
   return (
     <ThemeProvider defaultTheme="system" storageKey="theme">
-      <div className="min-h-screen bg-background text-foreground">
-        <div className="m-5">
-          <ModeToggle />
-        </div>
-        <Routes>
-          {/* <Route index element={<Home />} />
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarTrigger />
+
+        <div className="min-h-screen bg-background text-foreground">
+          <div className="m-5">
+            <ModeToggle />
+          </div>
+          <Routes>
+            {/* <Route index element={<Home />} />
           <Route path="about" element={<About />} /> */}
 
-          <Route>
-            <Route path="home" element={<ProtectedRoute />} />
-            <Route path="login" element={<LoginPage />} />
-            <Route path="register" element={<RegisterPage />} />
-          </Route>
+            <Route>
+              <Route path="home" element={<ProtectedRoute />} />
+              <Route path="login" element={<LoginPage />} />
+              <Route path="register" element={<RegisterPage />} />
+            </Route>
 
-          {/* <Route path="concerts">
+            {/* <Route path="concerts">
             <Route index element={<ConcertsHome />} />
             <Route path=":city" element={<City />} />
             <Route path="trending" element={<Trending />} />
           </Route> */}
-        </Routes>
-      </div>
+          </Routes>
+        </div>
+      </SidebarProvider>
     </ThemeProvider>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,19 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar.tsx";
-import { AppSidebar } from "./components/app-sidebar.tsx";
+
 // import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 // import { AppSidebar } from "@/components/layout/Sidebar";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarTrigger />
-        <App />
-      </SidebarProvider>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
